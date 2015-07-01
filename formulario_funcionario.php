6fd9b837b186cffa9f5fc2a5928a6014feeaceb4
pedromarques360@gmail.com <pedromarques360@gmail.com>
2015-07-01 01:36:16 -0300

    modificações nos formularios

--- formulario_funcionario.php
+++ formulario_funcionario.php
@@ -9,14 +9,14 @@
 
 	<form name="formulario_funcionario" method="post" action="cadastro_funcionariobd.php">
 		
-			Nome: <input type="text" name="nome"><br/><br/>
-			Usuário:<input type="text" name="usuario"><br/><br/>
-			Senha: <input type="password" name="senha" placeholder="Email"><br/><br/>
-			E-mail: <input type="email" name="email"><br/><br/>
-			Cargo:<input type="text" name="cargo"><br/><br/>
-			Turno:<input type="text" name="turno"><br/><br/>
-			Endereço:<input type="text" name="endereco"><br/><br/>
-			Telefone para Contato:<input type="text" name="telefone"><br/><br/>
+			Nome: <input type="text" name="nome" placeholder="Nome Completo"><br/><br/>
+			Usuário:<input type="text" name="usuario" placeholder="Nome de Usuários"><br/><br/>
+			Senha: <input type="password" name="senha" placeholder="Senha de Acesso"><br/><br/>
+			E-mail: <input type="email" name="email" placeholder="E-mail para Contatos"><br/><br/>
+			Cargo:<input type="text" name="cargo" placeholder="Função na escola"><br/><br/>
+			Turno:<input type="text" name="turno" placeholder="Turno de trabalho"><br/><br/>
+			Logradouro:<input type="text" name="endereco" placeholder="Rua,Av"><br/><br/>
+			Telefone para Contato:<input type="text" name="telefone" placeholder="Telefone para Contato"><br/><br/>
 			<input type="submit" value="Cadastrar" name="submit">
 
 		</form>
